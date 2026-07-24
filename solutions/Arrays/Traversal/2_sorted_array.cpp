fe8315daf31593180eb98c63fc656fd6a3bb9b8e
SrivatsalyaBhavaraju <srivatsalyabhavaraju@gmail.com>
2026-07-24 21:09:50 +0530

    traversal folder

---
+++ solutions/Arrays/Traversal/2_sorted_array.cpp
@@ -0,0 +1,15 @@
+class Solution{
+    public:
+    bool sortedArray(vector <int> &nums){
+        int n=nums.size();
+        if(n<=1){
+            return true;
+        }
+        for(int i=1;i<n;i++){
+            if(nums[i]<nums[i-1]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
