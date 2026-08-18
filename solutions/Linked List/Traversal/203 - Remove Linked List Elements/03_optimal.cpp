153aeae44705f343c0f01bfd30abf57ad82adb60
Srivatsalya Bhavaraju <srivatsalyabhavaraju@gmail.com>
2026-08-18 16:21:29 +0530

    ✨ Added Optimal solution for 203 - Remove Linked List Elements

---
+++ solutions/Linked List/Traversal/203 - Remove Linked List Elements/03_optimal.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        
+        if(head==nullptr) return head;
+        ListNode* prev=nullptr;
+        while (head != nullptr && head->val == val) {
+    head = head->next;
+}
+ListNode* temp=head;
+        while(temp!=nullptr){
+            
+            if(temp->val==val){
+                prev->next=temp->next;
+                temp=prev->next;
+
+            }else{
+                prev=temp;
+                temp=temp->next;
+            }
+        }
+        return head;
+    }
+};
